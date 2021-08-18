0f43eabf071f9279710c4c64a9f0df7a80d55f42
Mohamed Ismail <ismailreality1@gmail.com>
2021-08-18 17:46:42 +0200

    feat: cleanup App component

--- src/App.js
+++ src/App.js
@@ -1,33 +1,15 @@
-import { useEffect } from 'react';
 import {
   BrowserRouter as Router,
   Switch,
   Route,
 } from "react-router-dom";
 
-import useFetch from './hooks/useFetch';
-
-import { URLS } from './consts';
 import './App.css';
 
 import Home from './components/Home';
 import MovieOverview from './components/MovieOverview';
 
 function App() {
-  useEffect(() => {
-    const {
-      REACT_APP_API_BASE_URL,
-      REACT_APP_API_BASE_VERSION,
-      REACT_APP_API_KEY,
-    } = process.env;
-    fetch(`${REACT_APP_API_BASE_URL}/${REACT_APP_API_BASE_VERSION}/movie/now_playing?api_key=${REACT_APP_API_KEY}&language=en-US`)
-      .then(res => res.json())
-      .then(data => console.log(data));
-  }, []);
-
-  const {data, error, loading} = useFetch(URLS.CONFIGURATION);
-  console.log(data, error, loading);
-
   return (
     <Router>
       <div className="app">
