c506e7e2b9b783e55a1d4ee165fdc592b2b9fdbd
Mohamed Ismail <ismailreality1@gmail.com>
2021-08-18 18:46:20 +0200

    feat: implement basic movie overview details

--- src/components/MovieOverview/index.js
+++ src/components/MovieOverview/index.js
@@ -1,9 +1,45 @@
-import {  useParams } from "react-router-dom";
+import {  useParams } from 'react-router-dom';
+
+import { URLS } from '../../consts';
+
+import useFetch from '../../hooks/useFetch';
+
+import './index.css';
 
 export default function MovieOverview() {
   const { id } = useParams();
+  const url = `${URLS.MOVIE_DETAILS}/${id}`;
 
+  const { data } = useFetch(url);
+
+  const {
+    title,
+    overview,
+    backdrop_path,
+    poster_path,
+    genres,
+  } = data || {};
+
+  const genreNames = genres?.map(({ name }) => name).join(', ');
   return (
-    <div>{id}</div>
+    <div className="movie__overview">
+      <div className="movie__banner" style={{ backgroundImage: `url(https://image.tmdb.org/t/p/w1280${backdrop_path})` }}>
+        <div className="movie__meta-info">
+          <img
+            src={`https://image.tmdb.org/t/p/w500${poster_path}`}
+            alt={title}
+            className="movie__poster"
+          />
+          <div className="move-info">
+            <h2 className="movie-info__title">{title}</h2>
+            <p className="movie-info__genres">
+              <strong>Genres:</strong> {genreNames}
+            </p>
+            <p>{overview}</p>
+          </div>
+        </div>
+      </div>
+
+    </div>
   );
 }
